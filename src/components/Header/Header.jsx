40e94dabae67a810be4e32b8ac20af22374a0277
Oleksii Losiev <75750709+alexlex7@users.noreply.github.com>
2022-07-20 15:50:32 +0300

    added styles for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,36 +3,57 @@ import styles from './Header.module.css';
 import Container from '../Container/Container';
 import { useMediaQuery } from 'react-responsive';
 import MobileMenu from '../MobileMenu/MobileMenu';
+import { ReactComponent as SearchIcon } from './images/searchIcon.svg';
+import { ReactComponent as MenuIcon } from './images/menu.svg';
 
 export default function Header() {
-  const isMobile = useMediaQuery({ maxWidth: 589 });
+  const isMobile = useMediaQuery({ maxWidth: 1204 });
 
   return (
     <header className={styles.header}>
       <Container classNames={styles.container}>
-        <div className={styles.logo}>DudeShape</div>
+        <div className={styles.logoWrapper}>
+          <a className={styles.logo} href="#">
+            DudeShape
+          </a>
+        </div>
         {isMobile ? (
           <MobileMenu />
         ) : (
-          <nav>
-            <ul className={styles.list}>
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Features</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-              <li>
-                <a href="#">Search</a>
-              </li>
-            </ul>
-          </nav>
+          <>
+            <nav>
+              <ul className={styles.list}>
+                <li className={styles.listItem}>
+                  <a href="#" className={styles.navLink}>
+                    Home
+                  </a>
+                </li>
+                <li className={styles.listItem}>
+                  <a href="#" className={styles.navLink}>
+                    About
+                  </a>
+                </li>
+                <li className={styles.listItem}>
+                  <a href="#" className={styles.navLink}>
+                    Features
+                  </a>
+                </li>
+                <li className={styles.listItem}>
+                  <a href="#" className={styles.navLink}>
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <div className={styles.wrapper}>
+              <button className={styles.btn} type="button">
+                <SearchIcon className={styles.icon} />
+              </button>
+              <button className={styles.btn} type="button">
+                <MenuIcon className={styles.icon} />
+              </button>
+            </div>
+          </>
         )}
       </Container>
     </header>
