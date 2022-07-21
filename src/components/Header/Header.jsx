5955a5052865216fac02fb0a05d4a74b529848a8
Oleksii Losiev <75750709+alexlex7@users.noreply.github.com>
2022-07-21 10:01:33 +0300

    changed drop out menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.css';
 import Container from '../Container/Container';
 import { useMediaQuery } from 'react-responsive';
-import MobileMenu from '../MobileMenu/MobileMenu';
 import { ReactComponent as SearchIcon } from './images/searchIcon.svg';
 import { ReactComponent as MenuIcon } from './images/menu.svg';
+import { ReactComponent as MobileMenuIcon } from './images/mobileMenuIcon.svg';
 
 export default function Header() {
   const isMobile = useMediaQuery({ maxWidth: 1204 });
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((isOpen) => !isOpen);
+  };
 
   return (
     <header className={styles.header}>
@@ -18,7 +23,46 @@ export default function Header() {
           </a>
         </div>
         {isMobile ? (
-          <MobileMenu />
+          <div className={styles.menu}>
+            <button
+              type="button"
+              onClick={toggleMenu}
+              className={styles.menuBtn}
+            >
+              <MobileMenuIcon className={styles.mobileMenuIcon} />
+            </button>
+            {isMenuOpen && (
+              <div className={styles.dropOut}>
+                <ul className={styles.dropOutList}>
+                  <li>
+                    <a href="#" className={styles.dropOutLink}>
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className={styles.dropOutLink}>
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className={styles.dropOutLink}>
+                      Features
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className={styles.dropOutLink}>
+                      Contact
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className={styles.dropOutLink}>
+                      Search
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         ) : (
           <>
             <nav>
