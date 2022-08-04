02c9e6da1671096d8bf92957d83d02024032805b
Oleksii Losiev <75750709+alexlex7@users.noreply.github.com>
2022-08-04 16:58:20 +0300

    added modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { createPortal } from 'react-dom';
+import styles from './Modal.module.css';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export default function Modal({ isModalOpen, toggleModal, children }) {
+  const handleClick = (e) => {
+    if (e.target === e.currentTarget) {
+      toggleModal();
+    }
+  };
+
+  return createPortal(
+    <AnimatePresence>
+      {isModalOpen && (
+        <motion.div
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+            transition: {
+              duration: 1,
+            },
+          }}
+          exit={{
+            opacity: 0,
+            transition: {
+              delay: 1.5,
+              duration: 1,
+            },
+          }}
+          className={styles.overlay}
+          onClick={handleClick}
+        >
+          <motion.div
+            initial={{
+              x: 1200,
+              scale: 0.5,
+            }}
+            animate={{
+              x: 0,
+              scale: 1,
+              transition: {
+                delay: 1,
+                duration: 1.5,
+              },
+            }}
+            exit={{
+              x: 1200,
+              scale: 0.5,
+              transition: {
+                duration: 1.5,
+              },
+            }}
+            className={styles.modalContainer}
+          >
+            <motion.div className={styles.modalContent}>{children}</motion.div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>,
+    modalRoot
+  );
+}
