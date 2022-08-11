b40abcab2a47c10fe6e3acfdc412119074a1e8c1
Oleksii Losiev <75750709+alexlex7@users.noreply.github.com>
2022-08-11 15:02:05 +0300

    add page navigation

--- src/components/NavListItem/NavListItem.jsx
+++ src/components/NavListItem/NavListItem.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import styles from './NavListItem.module.css';
 import PropTypes from 'prop-types';
+import { Link } from 'react-scroll';
 
 export default function NavListItem({ to, text }) {
   return (
     <li className={styles.listItem}>
-      <NavLink
+      {/* <NavLink
         to={to}
         className={({ isActive }) => (isActive ? styles.active : styles.item)}
       >
         {text}
-      </NavLink>
+      </NavLink> */}
+      <Link
+        className={styles.item}
+        to={to}
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+        // onSetActive={this.handleSetActive}
+      >
+        {text}
+      </Link>
     </li>
   );
 }
